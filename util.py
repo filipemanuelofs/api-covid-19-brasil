dc8d32c7e0289bc49bdcd8cbd57269435edb65fe
Filipe Manuel <filipemanuelofs@gmail.com>
2020-03-31 19:37:02 -0300

    Adição da função para formatar numeral com ponto

--- util.py
+++ util.py
@@ -10,3 +10,23 @@ def validar_data(data):
         return datetime.strptime(data, '%Y%m%d')
     except ValueError:
         raise ValueError('Formato da data incorreto, deve ser YYYYMMDD.')
+
+
+def formatar_numeral(numero, separador='.'):
+    def reverse(string):
+        string = "".join(reversed(string))
+        return string
+
+    s = reverse(str(numero))
+    count = 0
+    result = ''
+    for char in s:
+        count = count + 1
+        if count % 3 == 0:
+            if len(s) == count:
+                result = char + result
+            else:
+                result = separador + char + result
+        else:
+            result = char + result
+    return result
