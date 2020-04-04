78147fb114aed4b6f2ce4a547be60c0c55d70db9
Filipe Manuel <filipemanuelofs@gmail.com>
2020-04-04 20:30:05 -0300

    Correção da porta

--- main.py
+++ main.py
@@ -161,5 +161,5 @@ def get_index():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
